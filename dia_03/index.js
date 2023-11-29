964f144c2c39b917daad5c2a50e6230dd798312b
Samuel Cedric <samuelcedricc@gmail.com>
2023-11-29 20:28:28 -0300

    Resolução do exercício do dia #3.

--- dia_03/index.js
+++ dia_03/index.js
@@ -1,1 +1,9 @@
-console.log('hopi ha')
+let nome = prompt(`Desafio dia #3 - Conhecendo o Usuário!
+
+Insira seu nome:`)
+let idade = Number(prompt('Insira sua idade:'))
+let altura = parseFloat(prompt('Insira sua altura: (Exemplo: 1.70)'))
+let peso = parseFloat(prompt('Insira seu peso: (Exemplo: 64.2)'))
+let imc = peso / (altura*altura)
+
+console.log(`Olá ${nome}, você tem ${idade} anos, nasceu em ${2023 - idade}, tem ${altura.toFixed(2)} de altura, pesa ${peso.toFixed(2)}kg, seu IMC é ${imc.toFixed(2)}kg/m²`)
