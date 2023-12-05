69c0354e5534243e68aade5bb5a58a14475fc542
Samuel Cedric <samuelcedricc@gmail.com>
2023-12-05 16:25:04 -0300

    Resolução exercícios dia_05

---
+++ dia_05/index.js
@@ -0,0 +1,61 @@
+/*
+let num1 = Number(prompt(`Desafio dia #5: Exercício 1 - Calculadora DoDev.
+
+Insira qualquer número, sendo inteiro ou não.`))
+
+let num2 = Number(prompt(`Agora insira o segundo número.`))
+
+let operacao = Number(prompt(`Escolha 1 para somar;
+
+2 para subtrair;
+
+3 para multiplicar;
+
+ou 4 para dividir.`))
+
+switch (operacao){
+    case 1:
+        console.log(Number(`${num1 + num2}`))
+        break;
+    case 2:
+        console.log(Number(`${num1 - num2}`))
+        break;
+    case 3:
+        console.log(Number(`${num1 * num2}`))
+        break;
+    case 4:
+        console.log(Number(`${num1 / num2}`))
+        break;
+    default:
+        console.log(`Você inseriu um número errado :(`)
+        break;           
+}*/
+
+/*
+let opcao = Number(prompt(`Desafio dia #5: Exercício 2 - DoDev Thru.
+
+Escolha 1 para abastecer com gasolina;
+
+2 para abastecer com álcool;
+
+ou 3 para calibrar o pneus.`))
+
+switch (opcao){
+    case 1:
+        let gasolina = Number(prompt(`O litro da gasolina está R$5,00, qual o valor do abastecimento?
+        
+        *Use ponto ao invés de vírgula. Ex: R$142.50`))
+        
+        console.log(`Foram abastecidos ${gasolina / 5} litros` )
+        break;
+    case 2:
+        let alcool = Number(prompt(`O litro do álcool está R$3,00, qual o valor do abastecimento?
+        
+        *Use ponto ao invés de vírgula. Ex: R$142.50`))
+        
+        console.log(`Foram abastecidos ${alcool / 3} litros` )
+        break;
+    case 3:
+        console.log('Pneus calibrados com sucesso!')
+        break;
+}*/
