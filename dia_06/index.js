f69e91a6db166fd1947afd1b54e08023b0eeaa67
Samuel Cedric <samuelcedricc@gmail.com>
2023-12-08 19:53:11 -0300

    Estrutura FOR/Exercícios

---
+++ dia_06/index.js
@@ -0,0 +1,32 @@
+/*//Exercício 1:
+
+let num = Number(prompt(`Insira um número inteiro e positivo:`))
+
+for (let i = 0; i <= num; i++){
+    console.log(i)
+}*/
+
+
+/*//Exercício 2:
+
+for (let i = 0; i <= 50; i += 5){
+    console.log(i)
+}*/
+
+
+/*//Exercício 3:
+
+for (let i = 50; i >= 0; i -= 5){
+    console.log(i)
+}*/
+
+/*//Exercício 4:
+
+let num = Number(prompt(`Insira um número para ver a tabuada dele e dos dois seguintes:`))
+
+for (let tabuada = num; tabuada <= num + 2; tabuada++){
+    console.log(`Tabuada do número ${tabuada}`)
+    for (let mult = 0; mult <= 10; mult++){
+        console.log(`${tabuada} x ${mult} = ${tabuada * mult}`)
+    }
+}*/
